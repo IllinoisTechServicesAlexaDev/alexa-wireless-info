fa35bb482d1650cfd33a92b34b31792fe1b519ac
Althea <lhliu2@illinois.edu>
2018-07-24 13:49:48 -0500

    Update search_wirelesschecker.py

--- search_wirelesschecker.py
+++ search_wirelesschecker.py
@@ -31,3 +31,24 @@ def search_busy_building():
 			results.append(wirelesschecker['buildingname'])
 	return results
 
+def search_most_connection():
+	file = open('data/wirelesschecker.json', 'r')
+	wirelesscheckers = json.load(file)['data']
+	results = []
+	data = {}
+	for wirelesschecker in wirelesscheckers:
+		buildingname = wirelesschecker['buildingname']
+		clientdevice = int(wirelesschecker['clientdevices'])
+		totalAP = int(wirelesschecker['totalAP'])			
+		data[buildingname] = clientdevice
+
+	sorted_by_value = sorted(data.items(), key=lambda kv: kv[1], reverse=True)
+	building_1 = (sorted_by_value[0][0])
+	number_1 = (sorted_by_value[0][1])
+	building_2 = (sorted_by_value[1][0])
+	number_2 = (sorted_by_value[1][1])
+	building_3 = (sorted_by_value[2][0])
+	number_3 = (sorted_by_value[2][1])
+	
+	results = [building_1, number_1, building_2, number_2, building_3, number_3]
+	return(results)
